122dab29f125e11b732ed3b6dbf280a6aa47bdfc
Kirushanthy Jeyarajah <39116761+kirushanthy@users.noreply.github.com>
2020-01-11 13:57:23 +0530

    textMobile repo

--- src/main/java/com/example/ocr/Repository/OcrTextMobileRepository.java
+++ src/main/java/com/example/ocr/Repository/OcrTextMobileRepository.java
@@ -1,6 +1,8 @@
 package com.example.ocr.Repository;
+import com.example.ocr.Model.OcrTextMobile;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface OcrTextMobileRepository{
+public interface OcrTextMobileRepository extends CrudRepository<OcrTextMobile,Integer> {
 }
