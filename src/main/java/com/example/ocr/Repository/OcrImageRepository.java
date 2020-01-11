c4d2afd1e4a38f05748e88b16b4ebc86f55382e9
Kirushanthy Jeyarajah <39116761+kirushanthy@users.noreply.github.com>
2020-01-11 14:00:26 +0530

    image repo

--- src/main/java/com/example/ocr/Repository/OcrImageRepository.java
+++ src/main/java/com/example/ocr/Repository/OcrImageRepository.java
@@ -1,8 +1,8 @@
 package com.example.ocr.Repository;
-
-
+import com.example.ocr.Model.OcrImage;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface OcrImageRepository{
+public interface OcrImageRepository extends CrudRepository<OcrImage,Integer> {
 }
